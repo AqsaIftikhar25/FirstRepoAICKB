4a793b351c683502c71ae71963e3a0a827fc9be4
Aqsa Iftikhar <aqsaciit@gmail.com>
2020-09-15 20:04:57 +0200

    notes resolved

--- R/data_Dij.R
+++ R/data_Dij.R
@@ -1,5 +1,5 @@
 #' data from Wikipedia
-#' 
+#'
 #' a dataset containing two variables
 #'
 #'@format a data frame with 18 lines and 3 variables:
@@ -7,8 +7,9 @@
 #'\item{v1}{v1...}
 #'\item{V2}{v2...}
 #'\item{W}{W...}
+#'}
 #'
-#'
-#'@source \url{http://www.diamondse.info/}
+#'@references \url{https://en.wikipedia.org/wiki/Dijkstra\%27s}
 #'
 "wiki_graph"
+
